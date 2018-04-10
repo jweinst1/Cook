d5d85621ef7b30fd5c9afa5aa0b5107536e232c9
Joshua Weinstein <jweinstein@splunk.com>
2018-04-09 17:01:57 -0700

    compound header

---
+++ include/CookCompound.h
@@ -0,0 +1,12 @@
+#ifndef COOK_COMPOUND_HEAD
+#define COOK_COMPOUND_HEAD
+// Cook compound, primary value in Cook language
+
+// Cook elements act as individual atoms of Cook compounds,
+// Elements are analogous to types.
+typedef enum
+{
+        CookElement_None
+} CookElement;
+
+#endif

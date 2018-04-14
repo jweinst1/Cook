6df15521b5db2c9ef360fc16df856893f5dd6002
Joshua Weinstein <jweinstein@splunk.com>
2018-04-14 16:56:36 -0700

    doubly linked list, random access not needed.

--- include/CookAtom.h
+++ include/CookAtom.h
@@ -7,18 +7,38 @@
 
 // Base struct signifier for polymorphism
 // This may at some point hold more modifiers
-#define CookAtom_BASE CookElem elem;
+#define CookAtom_BASE CookElem elem; \
+        struct CookElem* prev; \
+        struct CookElem* next;
 
 // Macro to make a base atom with some element.
 #define CookAtom_MAKE(ptr, element) \
         ptr = malloc(sizeof(CookAtom)); \
         ptr->elem = element
 
-typedef struct
+// Connects two atoms
+#define CookAtom_CONNECT(at1, at2) \
+        at1->next = at2; \
+        at2->prev = at1
+
+#define CookAtom_DISCON(at1, at2) \
+        at1->next = NULL; \
+        at2->prev = NULL
+
+#define CookAtom_NULL(atom) \
+        atom->next = NULL; \
+        atom->prev = NULL
+
+#define CookAtomList_NULL(cal) \
+        cal->front = NULL; \
+        cal->back = NULL
+
+struct CookAtom
 {
         // Always at top of struct for same byte offset
         CookAtom_BASE
-} CookAtom;
+};
+typedef struct CookAtom CookAtom;
 
 typedef struct
 {
@@ -31,6 +51,13 @@ typedef struct
         CookAtom_BASE
 } CookAtom_plus;
 
+// Doubly linked list for storing same element atoms in a compound.
+typedef struct
+{
+        CookAtom* front;
+        CookAtom* back;
+        int len;
+} CookAtomList;
 
 CookAtom* CookAtom_new_int(int amount);
 
--- src/compound/CookAtom.c
+++ src/compound/CookAtom.c
@@ -2,8 +2,9 @@
 
 CookAtom* CookAtom_new_int(int amount)
 {
-        CookAtom* newatom = malloc(sizeof(CookAtom_int));
+        CookAtom_int* newatom = malloc(sizeof(CookAtom_int));
+        CookAtom_NULL(newatom);
         newatom->elem = CookElem_int;
         newatom->val = amount;
-        return newatom;
+        return (CookAtom*)newatom;
 }
